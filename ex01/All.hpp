8f32bf5cddef6bcd78d87ca0304d85ea5d574bfe
Chaimae Zghoumi <czghoumi@c1r6p7.1337.ma>
2025-10-10 18:32:40 +0100

    done

---
+++ ex01/All.hpp
@@ -0,0 +1,138 @@
+#ifndef ALL_HPP
+#define ALL_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+// using namespace std;
+class Contact
+{
+    private:
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string numb;
+    std::string darkest_secret;
+    public:
+   
+    std::string getfirst_name() 
+        {return first_name;}
+    void setfirst_name(std::string first) 
+        {first_name = first;}
+    // ------
+    std::string getlast_name() 
+        {return last_name;}
+    void setlast_name(std::string last) 
+        {last_name = last;}
+    // ------
+    std::string getnicknam() 
+        {return nickname;}
+    void setnicknam(std::string nick) 
+        {nickname = nick;}
+    // ------
+    std::string getNumb() 
+        {return numb;}
+    void setNumb(std::string a) 
+        {numb = a;}
+    // ------
+    std::string getdarkest_secret() 
+        {return darkest_secret;}
+    void setName(std::string darkest) 
+        {darkest_secret = darkest;}
+    // ------ 
+    // Constructor
+    Contact(std::string first,std::string last,std::string nick, std::string a,std::string darkest) 
+        { first_name = first; last_name=last;nickname=nick; numb = a;darkest_secret=darkest; }
+    // ------
+    void showdisplay()  {
+        std::cout << "first name: " << first_name << std::endl << "last name: " << last_name << std::endl << "nickname: " << nickname << std::endl << "numb: " << numb << std::endl << "darkest secret: " << darkest_secret << std::endl;
+    }
+    ~Contact() {
+    // std::std::cout << "Contact destroyed: " << first_name << std::endl;
+    }
+
+};
+
+
+class PhoneBook {
+private:
+    std::vector<Contact> contacts;
+public:
+  void addContact(const Contact& newContact) {
+        contacts.push_back(newContact);
+    }
+    
+    void setContact(size_t i, Contact c) {
+        if (i >= 0 && i < contacts.size()) contacts[i] = c;
+    }
+    void showClass() 
+    {
+        std::cout << "     index| firstname|  lastname|  nickname|" << std::endl;
+        for (size_t i = 0; i < contacts.size(); i++) 
+        {
+            int s = 0;
+            while(s < 9)
+            {
+                std::cout << " ";
+                s++;
+            }
+            std::cout << i << "|";
+            // -------
+            if (contacts[i].getfirst_name().size() <= 10)
+            {
+                int j = 10 - contacts[i].getfirst_name().size();
+                s = 0;
+                while(s < j)
+                {
+                   std::cout << " ";
+                   s++;
+                }
+                std::cout << contacts[i].getfirst_name() << "|";
+            }
+            else
+            {
+                std::cout << contacts[i].getfirst_name().substr(0, 9) + "." << "|";
+            }
+            // ------
+             if (contacts[i].getlast_name().size() <= 10)
+            {
+                int j = 10 - contacts[i].getlast_name().size();
+                s = 0;
+                while(s < j)
+                {
+                   std::cout << " ";
+                   s++;
+                }
+                std::cout << contacts[i].getlast_name() << "|";
+            }
+            else
+            {
+                std::cout << contacts[i].getlast_name().substr(0, 9) + "." << "|";
+            }
+            // -----
+             if (contacts[i].getnicknam().size() <= 10)
+            {
+                int j = 10 - contacts[i].getnicknam().size();
+                s = 0;
+                while(s < j)
+                {
+                   std::cout << " ";
+                   s++;
+                }
+                std::cout << contacts[i].getnicknam() << "|" << std::endl;
+            }
+            else
+            {
+               std::cout << contacts[i].getnicknam().substr(0, 9) + "." << "|" << std::endl;
+            }
+        }
+        
+    }
+    void getContact(int index)
+    {
+            contacts[index].showdisplay();
+    } 
+};
+
+#endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,138 +1,4 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <cctype>
-
-using namespace std;
-
-class Contact
-{
-    private:
-    string first_name;
-    string last_name;
-    string nickname;
-    string numb;
-    string darkest_secret;
-    public:
-   
-    string getfirst_name() 
-        {return first_name;}
-    void setfirst_name(string first) 
-        {first_name = first;}
-    // ------
-    string getlast_name() 
-        {return last_name;}
-    void setlast_name(string last) 
-        {last_name = last;}
-    // ------
-    string getnicknam() 
-        {return nickname;}
-    void setnicknam(string nick) 
-        {nickname = nick;}
-    // ------
-    string getNumb() 
-        {return numb;}
-    void setNumb(string a) 
-        {numb = a;}
-    // ------
-    string getdarkest_secret() 
-        {return darkest_secret;}
-    void setName(string darkest) 
-        {darkest_secret = darkest;}
-    // ------ 
-    // Constructor
-    Contact(string first,string last,string nick, string a,string darkest) 
-        { first_name = first; last_name=last;nickname=nick; numb = a;darkest_secret=darkest; }
-    // ------
-    void display() const {
-        cout << "first name: " << first_name << endl << "last name: " << last_name << endl << "nickname: " << nickname << endl << "numb: " << numb << endl << "darkest secret: " << darkest_secret << endl;
-    }
-    ~Contact() {
-    // std::cout << "Contact destroyed: " << first_name << std::endl;
-    }
-
-};
-
-
-class PhoneBook {
-private:
-    std::vector<Contact> contacts;
-public:
-  void addContact(const Contact& newContact) {
-        contacts.push_back(newContact);
-    }
-    
-    void setContact(size_t i, Contact c) {
-        if (i >= 0 && i < contacts.size()) contacts[i] = c;
-    }
-    void showClass() 
-    {
-        std::cout << "     index| firstname|  lastname|  nickname|" << std::endl;
-        for (size_t i = 0; i < contacts.size(); i++) 
-        {
-            int s = 0;
-            while(s < 9)
-            {
-                cout << " ";
-                s++;
-            }
-            cout << i << "|";
-            // -------
-            if (contacts[i].getfirst_name().size() <= 10)
-            {
-                int j = 10 - contacts[i].getfirst_name().size();
-                s = 0;
-                while(s < j)
-                {
-                   cout << " ";
-                   s++;
-                }
-                cout << contacts[i].getfirst_name() << "|";
-            }
-            else
-            {
-                cout << contacts[i].getfirst_name().substr(0, 9) + "." << "|";
-            }
-            // ------
-             if (contacts[i].getlast_name().size() <= 10)
-            {
-                int j = 10 - contacts[i].getlast_name().size();
-                s = 0;
-                while(s < j)
-                {
-                   cout << " ";
-                   s++;
-                }
-                cout << contacts[i].getlast_name() << "|";
-            }
-            else
-            {
-                cout << contacts[i].getlast_name().substr(0, 9) + "." << "|";
-            }
-            // -----
-             if (contacts[i].getnicknam().size() <= 10)
-            {
-                int j = 10 - contacts[i].getnicknam().size();
-                s = 0;
-                while(s < j)
-                {
-                   cout << " ";
-                   s++;
-                }
-                cout << contacts[i].getnicknam() << "|" << endl;
-            }
-            else
-            {
-               cout << contacts[i].getnicknam().substr(0, 9) + "." << "|" << endl;
-            }
-        }
-        
-    }
-    void getContact(int index)
-    {
-            contacts[index].display();
-    } 
-};
+#include "All.hpp"
 
 void display()
 {
@@ -141,21 +7,21 @@ void display()
     std::cout << "YOUR CHOICE --> ";
 }
 
-
 int main()
 {
     std::string input;
     PhoneBook ph;
     int i = 0;
     int j = 0;
-    while (1)
+    bool ajs = true;
+    while (ajs)
     {
         display();
         std::getline(std::cin,input);
         if (std::cin.eof())
         {
             std::cout << "End of input reached.\n";
-            break;
+            ajs=false;
         }
         if("ADD" == input)
         {
@@ -210,12 +76,12 @@ int main()
             // ------
             
             Contact con(fi_name,la_name,ni_name,line,desec);
-            if(i==8)
+            if(i == 8)
             {
                 ph.setContact((size_t)j, con);
                 j++;
-                if(j==8)
-                    j=0;
+                if(j == 8)
+                    j = 0;
             }
             else
             {
@@ -228,16 +94,18 @@ int main()
             int index;
             ph.showClass();
             std::cout << "shoos ure index" << std::endl;
-            cin >> index;
-            while (cin.fail() || index < 0 || index >= i) 
+            std::cin >> index;
+            while (std::cin.fail() || index < 0 || index >= i) 
             {
-                cout << "Invalid index, please inter a corect one" << endl;
-                cin >> index;
-            }       
+                std::cout << "Invalid index, please inter a corect one" << std::endl;
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cin >> index;
+            }
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             ph.getContact(index);
         }
         else if ("EXIT" == input)
-            break;
+            ajs=false;
     }
     return 0;
 }
